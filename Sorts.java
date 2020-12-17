bfbec549fb579882be6b351aabb3d1774ed637a3
vli21 <vli21@stuy.edu>
2020-12-16 23:05:16 -0500

    figured out how to swap two elements in

--- Sorts.java
+++ Sorts.java
@@ -3,5 +3,12 @@ public class Sorts{
   *@postcondition The array will be modified such that the elements will be in increasing order.
   *@param data  the elements to be sorted.
   */
-  public static void bubbleSort(int[] data){  }
+  public static void bubbleSort(int[] data){
+    if (data[0]<data[1]){
+      int temp=0;
+      temp=data[1];
+      data[1]=data[0];
+      data[0]=temp;
+    }
+}
 }
